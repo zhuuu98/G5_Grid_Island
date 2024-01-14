718f6c28963a18c2023cfd3bc04dd48c41253f65
zhuuu98 <liao.yingzhu98@gmail.com>
2024-01-14 11:44:34 +0800

    1144

--- src/App.vue
+++ src/App.vue
@@ -14,6 +14,7 @@ import HelloWorld from './components/HelloWorld.vue'
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
         <RouterLink to="/product">Product</RouterLink>
+        <RouterLink to="/News">News</RouterLink>
       </nav>
     </div>
   </header>
---
+++ src/views/NewsView.vue
@@ -0,0 +1,5 @@
+<template>
+    <div class="news">
+      <h1>This is news page</h1>
+    </div>
+  </template>
