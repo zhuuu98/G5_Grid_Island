fcc2d93681ae3095ed00ff8fb03bb15e60cb8c6f
martina216 <10230260c@gmail.com>
2024-01-19 17:50:47 +0800

    import font awesome and modify signup page

--- src/router/index.js
+++ src/router/index.js
@@ -18,8 +18,8 @@ const router = createRouter({
       component: () => import('../views/AboutView.vue')
     },
     {
-      path: '/News',
-      name: 'News',
+      path: '/news',
+      name: 'news',
       component: () => import('../views/NewsView.vue')
     },
     {
--- src/views/SignupView.vue
+++ src/views/SignupView.vue
@@ -1,19 +1,53 @@
 <template>
-    <div class="signup">
+    <div class="signup" id="app">
         <div class="signup_main">
             <div class="signup_main_griddy">
                 <!-- <img src="../assets/images/griddy_orange.png"> -->
             </div>
             <h2>註冊會員</h2>
             <form action="">
-                <a href="" class="btn_lg">使用Gmail註冊</a>
-                <a href="" title="請至手機裝置操作">使用Line註冊</a>
-                <a href="">使用Email註冊</a>
-                <p class="signup_signUp_text">已經是會員了?<a href="">點我登入</a></p>
+                <font-awesome-icon icon="coffee" />
+                <RouterLink v-for="item in signupButton" :to ="item.link" class="btn_lg">{{item.title}}</RouterLink>
+                <!-- <a href="https://www.google.com/" title="請至手機裝置操作">使用Line註冊</a>
+                <a href="">使用Email註冊</a> -->
+                <p class="signup_signUp_text">
+                    已經是會員了?
+                    <RouterLink to ="./login">點我登入</RouterLink>
+                </p>
+                
+
             </form>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: 0,
+      signupButton:[{
+        title:'使用Gmail登入',
+        link:'https://www.google.com/',
+        icon:'',
+    },{
+        title:'使用Line登入',
+        link:'https://www.line.com/',
+        icon:'',
+    },{
+        title:'使用email登入',
+        link:'./signup2',
+        icon:'',
+    }],
+    };
+  },
+  methods: {},
+  mounted() {},
+  
+}
+</script>
+
+
 
 <style lang="scss">
 //
