b8ec2b0e8df6b47d7d68cb8e3f6e574dc9f687e7
zhuuu98 <liao.yingzhu98@gmail.com>
2024-01-21 16:00:42 +0800

    大量header更動(hover效果、蟲蟲跑步、漢堡叉叉、logo微調)

--- src/components/MainHeader.vue
+++ src/components/MainHeader.vue
@@ -31,9 +31,12 @@
               <!-- 漢堡圖標 -->
               <li>
                 <button @click="toggleMenu" class="ham_btn">
-                  <div class="ham_btn_line"></div>
+                  <!-- <div class="ham_btn_line1"></div>
                   <div class="ham_btn_line2"></div>
-                  <div class="ham_btn_line"></div>
+                  <div class="ham_btn_line3"></div> -->
+                  <div :class="{ 'active-line1': menuOpen }" class="ham_btn_line1"></div>
+                  <div :class="{ 'active-line2': menuOpen }" class="ham_btn_line2"></div>
+                  <div :class="{ 'active-line3': menuOpen }" class="ham_btn_line3"></div>
                 </button>
               </li>
             </ul>
@@ -43,9 +46,9 @@
           <!-- 選單內容 -->
           <div v-if="menuOpen" class="ham_menu">
 
-            <div class="ham_logo">
+            <RouterLink to="/" class="ham_logo">
               <img src="../assets/images/header/gdidlogobox.svg" alt="">
-            </div>
+            </RouterLink>
 
             <!-- 選單項目 -->
             <div class="ham_links">
@@ -53,18 +56,22 @@
               <RouterLink to="/product" class="ham_link">所有商品</RouterLink>
               <RouterLink to="/news" class="ham_link">最新消息</RouterLink>
               <!-- 玩家社群，含子階層 -->
-              <div class="ham_link ham_comm" @click="togglesubMenuOpen">
-                玩家社群
+              <div class="ham_comm" @click="toggleSubMenuOpen">
+                <div class="ham_link">玩家社群
+                  <font-awesome-icon :icon="['fas', 'angle-right']" class="ham_angle_right"/>
+                </div>
                 <ul class="submenu" v-show="subMenuOpen">
                   <li class="ham_sub_bug">
-                    <img src="../assets/images//header/griddy_orange.png" alt="am_sub_bug">
+                    <img src="../assets/images//header/griddy_orange.png" alt="am_sub_bug" class="running">
                   </li>
-                  <li class="ham_sub_comm">
-                    <RouterLink to="#">留言區</RouterLink>
-                  </li>
-                  <li class="ham_sub_comm">
-                    <RouterLink to="#">報隊區</RouterLink>
-                  </li>
+                  <div>
+                    <li class="ham_sub_comm">
+                      <RouterLink to="#">留言區</RouterLink>
+                    </li>
+                    <li class="ham_sub_comm">
+                      <RouterLink to="#">報隊區</RouterLink>
+                    </li>
+                  </div>
                 </ul>
               </div>
               <RouterLink to="/about" class="ham_link">關於我們</RouterLink>
@@ -73,6 +80,8 @@
             </div>
           </div>
         </transition>
+
+
 
       </div>
     </div>
@@ -89,13 +98,14 @@ export default {
     return {
       menuOpen: false, //漢堡開關
       subMenuOpen: false, //玩家社群子階層
+      hamX: false,
     };
   },
   methods: {
     toggleMenu() {
       this.menuOpen = !this.menuOpen;
     },
-    togglesubMenuOpen() {
+    toggleSubMenuOpen() {
       this.subMenuOpen = !this.subMenuOpen;
     },
   },
--- src/main.js
+++ src/main.js
@@ -17,7 +17,7 @@ import "view-ui-plus/dist/styles/viewuiplus.css";
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 
 // 引入需要的FontAwesome圖標
-import { library, config  } from '@fortawesome/fontawesome-svg-core';
+import { library, config,} from '@fortawesome/fontawesome-svg-core';
 
 import { fas } from '@fortawesome/free-solid-svg-icons'
 import { far } from '@fortawesome/free-regular-svg-icons'
