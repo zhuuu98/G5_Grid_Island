e3fecc49e06a04f40e4bea13136b0918c7ff2be3
cjun2351 <cjun2351@gmail.com>
2024-01-30 13:57:57 +0800

    增加商品波浪以及增加最新消息分頁資料

--- src/views/NewsArticleView.vue
+++ src/views/NewsArticleView.vue
@@ -2,10 +2,12 @@
   <main class="newsArticle">
     <div class="title">
       <h1>text</h1>
-      <h1>{{ respondData.news_title }}</h1>
+      <h1>{{ $route.params.id }}</h1>
     </div>
     <div class="newsImg">
-      <img :src="`https://tibamef2e.com/chd103/g1/image/news/${respondData.news_img}`">
+      <img
+        :src="`https://tibamef2e.com/chd103/g1/image/news/${respondData.news_img}`"
+      />
     </div>
     <div class="content">
       <span>{{ respondData.news_content }}</span>
@@ -13,34 +15,35 @@
   </main>
 </template>
 
-
 <script>
 import axios from "axios";
 import PageTitle from "../components/PageTitle.vue";
-
 
 export default {
   data() {
     return {
       respondData: [],
-
     };
   },
   components: {
     PageTitle,
   },
-  computed: {
-  },
+  computed: {},
   created() {
     this.axiosGetData();
   },
   methods: {
     axiosGetData() {
+      const pageId = this.$route.params.id;
       axios
         .get("https://tibamef2e.com/chd103/g1/phps/news_fetch.php")
         .then((res) => {
-          console.log(res.data);
-          this.respondData = res.data;
+          const allNews = res.data;
+          const result = allNews.find((item) => {
+            return item.news_id == pageId;
+          });
+          console.log(result);
+          this.respondData = result;
         });
     },
   },
