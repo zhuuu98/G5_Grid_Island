8bac3ac294aeb738b39bcdfb6b918e675b6a6176
morxdd <pomin1995qoot@gmail.com>
2024-02-01 05:48:12 +0800

    可正確切換與顯示膚色、眼睛、耳朵

--- src/components/styleComponents/skinComponent.vue
+++ src/components/styleComponents/skinComponent.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="componentsarea">
-        <div id="spot-box">
+        <div id="body-box">
             <h3>軀幹</h3>
             <ul class="body-options options-colors">
                 <li v-for="bodyColor in bodyColors" :key="bodyColor" :style="{ backgroundColor: bodyColor }"
@@ -8,10 +8,16 @@
                 </li>
             </ul>
         </div>
-    </div>
-    <div class="componentsarea">
+        <div id="belly-box">
+            <h3>肚肚</h3>
+            <ul class="belly-options options-colors">
+                <li v-for="bellyColor in bellyColors" :key="bellyColor" :style="{ backgroundColor: bellyColor }"
+                    @click="handleBellyColorChange(bellyColor)" :class="{ selected: selectedBellyColor === bellyColor }">
+                </li>
+            </ul>
+        </div>
         <div id="spot-box">
-            <h3>斑紋</h3>
+            <h3>斑點</h3>
             <ul class="spot-options options-colors">
                 <li v-for="spotColor in spotColors" :key="spotColor" :style="{ backgroundColor: spotColor }"
                     @click="handleSpotColorChange(spotColor)" :class="{ selected: selectedSpotColor === spotColor }">
@@ -26,35 +32,39 @@
 
     export default {
         name: 'SkinComponent',
-        props: {
-            spotColor: String
-        },
         setup(props, context) {
             // 使用reactive代替ref，创建一个反应性的state对象
-            const spotState = reactive({
-                selectedSpotColor: props.currentColor
-            });
             const bodyState = reactive({
                 selectedBodyColor: props.currentColor
             });
-
-            const spotColors = ['#d2eb86', '#1ca88e', '#fbb466', '#f9eaa7', '#f7f7f7', '#fb7676', '#ffdcdc', '#9dd0e1', '#a9aaff', '#c088f9'];
+            const bellyState = reactive({
+                selectedBellyColor: props.currentColor
+            });
+            const spotState = reactive({
+                selectedSpotColor: props.currentColor
+            });
             const bodyColors = ['#d2eb86', '#1ca88e', '#fbb466', '#f9eaa7', '#f7f7f7', '#fb7676', '#ffdcdc', '#9dd0e1', '#a9aaff', '#c088f9'];
+            const bellyColors = ['#d2eb86', '#1ca88e', '#fbb466', '#f9eaa7', '#f7f7f7', '#fb7676', '#ffdcdc', '#9dd0e1', '#a9aaff', '#c088f9']; 
+            const spotColors = ['#d2eb86', '#1ca88e', '#fbb466', '#f9eaa7', '#f7f7f7', '#fb7676', '#ffdcdc', '#9dd0e1', '#a9aaff', '#c088f9'];
 
             // 更新選中的颜色
-            const handleSpotColorChange = (spotColor) => {
-                context.emit('spot-color-selected', spotColor);
-                console.log("事件已發射，斑紋顏色：", spotColor);
-            };
             const handleBodyColorChange = (bodyColor) => {
-                console.log(bodyColor);
                 context.emit('body-color-selected', bodyColor);
                 console.log("事件已發射，軀幹顏色：", bodyColor);
             };
+            const handleBellyColorChange = (bellyColor) => {
+                context.emit('belly-color-selected', bellyColor);
+                console.log("事件已發射，肚肚顏色：", bellyColor);
+            };
+            const handleSpotColorChange = (spotColor) => {
+                context.emit('spot-color-selected', spotColor);
+                console.log("事件已發射，斑點顏色：", spotColor);
+            };
 
             return {
-                spotColors, selectedSpotColor: spotState.selectedSpotColor, handleSpotColorChange,
                 bodyColors, selectedBodyColor: bodyState.selectedBodyColor, handleBodyColorChange,
+                bellyColors, selectedBellyColor: bellyState.selectedBellyColor, handleBellyColorChange,
+                spotColors, selectedSpotColor: spotState.selectedSpotColor, handleSpotColorChange,
 
 
             };
