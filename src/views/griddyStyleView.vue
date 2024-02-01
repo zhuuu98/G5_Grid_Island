288a19d09255162e42af975afb045bb44777e498
morxdd <pomin1995qoot@gmail.com>
2024-02-01 11:39:06 +0800

    造型屋可以存圖片了

--- src/views/griddyStyleView.vue
+++ src/views/griddyStyleView.vue
@@ -1,45 +1,61 @@
 <template>
     <div id="style-body">
-        <div class="style-operate">
-            <!-- 預覽區組件，顯示選擇的零件以及操作按鈕 -->
-            <div class="style-view">
-                <!-- 傳遞顏色到SVG組件 -->
-                <div id="viewimagebox">
-                    <originalGriddySrc :selectedSpotColor="selectedSpotColor" :selectedBodyColor="selectedBodyColor"
-                        :selectedBellyColor="selectedBellyColor" 
-                        :selectedEyesStaff="selectedEyesStaff"
-                        :selectedEyesColor="selectedEyesColor"
-                        :selectedEarsStaff="selectedEarsStaff" 
-                        :selectedEarsColor="selectedEarsColor" 
-                        :selectedAccessoriesStaff="selectedAccessoriesStaff"
-                        :selectedBackgroundColor="selectedBackgroundColor"  />
+        <div id="style-play-zone" v-show="currentZone === 'style-play-zone'">
+            <div id="style-operate">
+                <!-- 預覽區組件，顯示選擇的零件以及操作按鈕 -->
+                <div class="style-view">
+                    <!-- 傳遞顏色到SVG組件 -->
+                    <div class="viewimagebox">
+                        <originalGriddySrc :selectedSpotColor="selectedSpotColor" :selectedBodyColor="selectedBodyColor"
+                            :selectedBellyColor="selectedBellyColor" :selectedEyesStaff="selectedEyesStaff"
+                            :selectedEyesColor="selectedEyesColor" :selectedEarsStaff="selectedEarsStaff"
+                            :selectedEarsColor="selectedEarsColor" :selectedAccessoriesStaff="selectedAccessoriesStaff"
+                            :selectedBackgroundColor="selectedBackgroundColor" />
+                    </div>
+                    <button id="setphoto" @click="toggleZone">確認送出</button>
+                </div>
+                <div class="style-btn">
+                    <!-- 操作按鈕 (如果有的話) -->
+
                 </div>
             </div>
-            <div class="style-btn">
-                <!-- 操作按鈕 (如果有的話) -->
+            <!-- 選項區，含頁籤 -->
+            <div class="stylearea">
+                <div class="tabs">
+                    <button v-for="tab in tabs" :key="tab.name" :class="{ active: currentTab === tab.component }"
+                        @click="currentTab = tab.component">
+                        {{ tab.title }}
+                    </button>
+                </div>
+                <!-- 頁籤內容 -->
+                <div class="tab-content">
+                    <component :is="currentTab" @spot-color-selected="handleSpotColorChange"
+                        @body-color-selected="handleBodyColorChange" @belly-color-selected="handleBellyColorChange"
+                        @eyes-color-selected="handleEyesColorChange" @eyes-staff-selected="handleEyesStaffChange"
+                        @ears-color-selected="handleEarsColorChange" @ears-staff-selected="handleEarsStaffChange"
+                        @accessories-staff-selected="handleAccessoriesStaffChange"
+                        @background-color-selected="handleBackgroundColorChange">
+                    </component>
+
+                </div>
             </div>
         </div>
-        <!-- 選項區，含頁籤 -->
-        <div class="stylearea">
-            <div class="tabs">
-                <button v-for="tab in tabs" :key="tab.name" :class="{ active: currentTab === tab.component }"
-                    @click="currentTab = tab.component">
-                    {{ tab.title }}
-                </button>
-            </div>
-            <!-- 頁籤內容 -->
-            <div class="tab-content">
-                <component :is="currentTab" @spot-color-selected="handleSpotColorChange"
-                    @body-color-selected="handleBodyColorChange" @belly-color-selected="handleBellyColorChange"
-                    @eyes-color-selected="handleEyesColorChange" @eyes-staff-selected="handleEyesStaffChange"
-                    @ears-color-selected="handleEarsColorChange" @ears-staff-selected="handleEarsStaffChange"
-                    @accessories-staff-selected="handleAccessoriesStaffChange"
-                    @background-color-selected="handleBackgroundColorChange">
-                </component>
-
+        <div id="show-zone" v-show="currentZone === 'show-zone'">
+            <div class="final-show">
+                <!-- 傳遞顏色到SVG組件 -->
+                <div id="finalimagebox">
+                    <originalGriddySrc :selectedSpotColor="selectedSpotColor" :selectedBodyColor="selectedBodyColor"
+                        :selectedBellyColor="selectedBellyColor" :selectedEyesStaff="selectedEyesStaff"
+                        :selectedEyesColor="selectedEyesColor" :selectedEarsStaff="selectedEarsStaff"
+                        :selectedEarsColor="selectedEarsColor" :selectedAccessoriesStaff="selectedAccessoriesStaff"
+                        :selectedBackgroundColor="selectedBackgroundColor" />
+                </div>
+                <p>最棒的Griddy粉墨登場！</p>
+                <img v-if="griddyImage" :src="griddyImage" alt="Captured Griddy Content">
+                <button @click="griddyToImage">下載圖片</button>
+                <button id="goback" @click="toggleZone">回上頁</button>
             </div>
         </div>
-
     </div>
 </template>
 
@@ -60,6 +76,7 @@
     import { earsStaffs } from "@/policy/color.js"
     import { accessoriesStaffs } from "@/policy/color.js"
     import { backgroundColors } from "@/policy/color.js"
+    import html2canvas from 'html2canvas';
 
 
     export default {
@@ -70,10 +87,10 @@
             earsComponent, //耳朵區
             accessoriesComponent, //配件區
             backgroundComponent, //背景顏色區
-            
-            
+
+
         },
-        
+
         setup() {
             const tabs = [
                 { name: 'tab1', title: '膚色', component: skinComponent },
@@ -92,12 +109,55 @@
             const selectedEarsColor = ref(earsColors[0]);
             const selectedAccessoriesStaff = ref(accessoriesStaffs[0]);
             const selectedBackgroundColor = ref(backgroundColors[4]);
+            const currentZone = ref('style-play-zone'); // 初始设为 true 或 false 根据您的需求
+            const toggleZone = () => {
+                currentZone.value = currentZone.value === 'style-play-zone' ? 'show-zone' : 'style-play-zone';
+            };
+            const griddyImage = ref(null); // 存储转换后的图像
+            const griddyToImage = () => {
+                const elementToCapture = document.getElementById('finalimagebox');
+                // 设置画布分辨率增加的倍数，例如 2 表示 2x 分辨率
+                const scale = 4;
+
+                // 设置 html2canvas 选项
+                const options = {
+                    scale: scale,
+                    width: elementToCapture.offsetWidth,
+                    height: elementToCapture.offsetHeight,
+                    useCORS: true // 允许加载跨域图片
+                };
+
+
+
+
+                html2canvas(elementToCapture, options).then(canvas => {
+                    const downloadLink = document.createElement('a');
+                    // 使用当前日期和时间来生成唯一的文件名
+                    const date = new Date();
+                    const timestamp = date.getFullYear().toString() +
+                        (date.getMonth() + 1).toString().padStart(2, '0') +
+                        date.getDate().toString().padStart(2, '0') + '_' +
+                        date.getHours().toString().padStart(2, '0') +
+                        date.getMinutes().toString().padStart(2, '0') +
+                        date.getSeconds().toString().padStart(2, '0');
+                    const filename = `OMG${timestamp}.png`;
+
+                    downloadLink.href = canvas.toDataURL('image/png');
+                    downloadLink.download = filename; // 图片文件名
+
+                    // 模拟点击链接以触发下载
+                    document.body.appendChild(downloadLink);
+                    downloadLink.click();
+                    document.body.removeChild(downloadLink);
+                });
+            };
 
 
             return {
-                tabs, currentTab, selectedBodyColor, selectedBellyColor, selectedSpotColor, selectedBellyColor,
+                currentZone, toggleZone, tabs, currentTab, selectedBodyColor, selectedBellyColor, selectedSpotColor, selectedBellyColor,
                 selectedEyesColor, selectedEyesStaff, selectedEarsColor, selectedEarsStaff,
-                selectedAccessoriesStaff, selectedBackgroundColor
+                selectedAccessoriesStaff, selectedBackgroundColor,
+                griddyToImage,
             };
         },
         methods: {
