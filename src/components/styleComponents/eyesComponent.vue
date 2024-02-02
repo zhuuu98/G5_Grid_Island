d6155b1a984cfc784bf1df764978dd6ec834ad82
morxdd <pomin1995qoot@gmail.com>
2024-02-03 03:55:30 +0800

    造型屋新增四對眼睛

--- src/components/styleComponents/eyesComponent.vue
+++ src/components/styleComponents/eyesComponent.vue
@@ -28,6 +28,10 @@
     import { eyesStaff4 } from "@/policy/color.js"
     import { eyesStaff5 } from "@/policy/color.js"
     import { eyesStaff6 } from "@/policy/color.js"
+    import { eyesStaff7 } from "@/policy/color.js"
+    import { eyesStaff8 } from "@/policy/color.js"
+    import { eyesStaff9 } from "@/policy/color.js"
+    import { eyesStaff10 } from "@/policy/color.js"
     import { unifiedColors } from "@/policy/color.js";
 
     export default {
@@ -41,7 +45,10 @@
                     { label: "哭哭", staff: eyesStaff4 },
                     { label: "水汪汪", staff: eyesStaff5 },
                     { label: "複眼", staff: eyesStaff6 },
-                    
+                    { label: "疲憊", staff: eyesStaff7 },
+                    { label: "舒眠", staff: eyesStaff8 },
+                    { label: "愛戀", staff: eyesStaff9 },
+                    { label: "少年", staff: eyesStaff10 },
                 ]
             };
         },
