e69f9dc70d3202f54749f154aacb8a70317002fb
martina216 <10230260c@gmail.com>
2024-02-03 23:52:51 +0800

    404頁面 優化，footer 海浪修正，圖片上傳

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,15 +1,25 @@
 <template>
-  <div>404 Not found</div>
-  <button @click="goHome">回首頁</button>
+  <div class="notFound">
+    <div class="content">
+      <div class="wrapper">
+        <img src="../assets/images/404/404_emptyHouse.svg" alt="404-GridIsland-empty-House">
+        <div class="textArea">
+          <div class="text">
+            <span>404</span><span>Not Found</span>
+          </div>
+          <button class="btn_secondary pc" @click="goHome">回首頁</button>
+        </div>
+      </div>
+      <button class="btn_lg_orange mobile" @click="goHome">回首頁</button>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
   methods: {
     goHome() {
-      this.$router.push({
-        name: "home",
-      });
+      this.$router.push("/");
     },
   },
 };
