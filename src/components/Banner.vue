01bac216f1d1c9e0d6e4f9a039d5356d4fb5a58f
morxdd <pomin1995qoot@gmail.com>
2024-02-06 01:54:01 +0800

    刪除HomeView_v2 // 首頁改寫，引入banner組件

--- src/components/Banner.vue
+++ src/components/Banner.vue
@@ -9,49 +9,66 @@
                 <img :src="islandTree" id="islandTree-image" @mouseenter="showTreeCard = true"
                     @mouseleave="showTreeCard = false" />
             </router-link>
-            
+
             <div id="newsTag" class="itemTag">
                 <p>最新消息</p>
             </div>
             <router-link to="/news">
                 <img :src="islandNews" id="islandNews-image" @mouseenter="showNewsCard = true"
-                @mouseleave="showNewsCard = false" />
+                    @mouseleave="showNewsCard = false" />
             </router-link>
 
             <img :src="islandBar" id="islandBar-image" />
 
+            <div id="GamesTag" class="itemTag">
+                <p>所有商品</p>
+            </div>
             <router-link to="/product">
                 <img :src="islandGames" id="islandGames-image" @mouseenter="showGamesCard = true"
                     @mouseleave="showGamesCard = false" />
             </router-link>
-
+            <div id="aboutTag" class="itemTag">
+                <p>關於我們</p>
+            </div>
             <img :src="islandRoof" id="islandRoof-image" />
-
+            <div id="boardTag" class="itemTag">
+                <p>玩家社群</p>
+            </div>
             <router-link to="/board">
                 <img :src="islandBoard" id="islandBoard-image" @mouseenter="showBoardCard = true"
-                @mouseleave="showBoardCard = false" />
+                    @mouseleave="showBoardCard = false" />
             </router-link>
+            <div id="bugTag" class="itemTag">
+                <p>Griddy<br>造型屋</p>
+            </div>
             <router-link to="/griddy-style">
                 <img :src="islandBug" id="islandBug-image" @mouseenter="showBugCard = true"
-                @mouseleave="showBugCard = false" />
+                    @mouseleave="showBugCard = false" />
             </router-link>
 
             <img :src="islandBarup" id="islandBarup-image" />
 
+            <div id="prebookTag" class="itemTag">
+                <p>場地預約</p>
+            </div>
             <router-link to="/book">
                 <img :src="islandPrebook" id="islandPrebook-image" @mouseenter="showPrebookCard = true"
-                @mouseleave="showPrebookCard = false" />
+                    @mouseleave="showPrebookCard = false" />
             </router-link>
+            <div id="cartTag" class="itemTag">
+                <p>購物車</p>
+            </div>
             <router-link to="/cart">
                 <img :src="islandCart" id="islandCart-image" @mouseenter="showCartCard = true"
-                @mouseleave="showCartCard = false" />
+                    @mouseleave="showCartCard = false" />
             </router-link>
+
             <router-link to="/about">
                 <img :src="islandAbout" id="islandAbout-image" @mouseenter="showAboutCard = true"
-                @mouseleave="showAboutCard = false" />
+                    @mouseleave="showAboutCard = false" />
             </router-link>
-            
-            <img :src="lEye" id="lEye" ref="leftEyeRef"/>
+
+            <img :src="lEye" id="lEye" ref="leftEyeRef" />
             <img :src="rEye" id="rEye" ref="rightEyeRef" />
             <!-- ============================================= -->
             <div id="banner-cardsbox">
@@ -147,84 +164,86 @@
                 showCartCard: false, //購物車，購物車
                 showBugCard: false, //造型屋，蟲
                 showNewsCard: false, //最新消息，佈告欄
+
             };
         },
         mounted() {
-			// 为整个容器添加事件监听器
-			this.$refs.bannerContainer.addEventListener("mousedown", this.preventDrag);
-			document.addEventListener("mousemove", this.handleMouseMove);
-		},
-		beforeDestroy() {
-			// 在组件销毁时移除事件监听器
-			this.$refs.bannerContainer.removeEventListener(
-				"mousedown",
-				this.preventDrag
-			);
-			document.removeEventListener("mousemove", this.handleMouseMove);
+            // 为整个容器添加事件监听器
+            this.$refs.bannerContainer.addEventListener("mousedown", this.preventDrag);
+            document.addEventListener("mousemove", this.handleMouseMove);
+            
+        },
+        beforeDestroy() {
+            // 在组件销毁时移除事件监听器
+            this.$refs.bannerContainer.removeEventListener(
+                "mousedown",
+                this.preventDrag
+            );
+            document.removeEventListener("mousemove", this.handleMouseMove);
 
-		},
+        },
         methods: {
             // 定義組件的方法
             preventDrag(event) {
-				// 检查事件的目标是否是您想阻止拖拽的图像
-				if (event.target.tagName === "IMG") {
-					event.preventDefault();
-				}
-			},
+                // 检查事件的目标是否是您想阻止拖拽的图像
+                if (event.target.tagName === "IMG") {
+                    event.preventDefault();
+                }
+            },
             moveEye(
-				eye,
-				mouseX,
-				mouseY,
-				initialLeft,
-				initialTop,
-				maxMovementX,
-				maxMovementY
-			){
-				if (!eye) return;
+                eye,
+                mouseX,
+                mouseY,
+                initialLeft,
+                initialTop,
+                maxMovementX,
+                maxMovementY
+            ) {
+                if (!eye) return;
 
-				const eyeRect = eye.getBoundingClientRect();
-				const eyeCenterX = eyeRect.left + eyeRect.width / 2;
-				const eyeCenterY = eyeRect.top + eyeRect.height / 2;
+                const eyeRect = eye.getBoundingClientRect();
+                const eyeCenterX = eyeRect.left + eyeRect.width / 2;
+                const eyeCenterY = eyeRect.top + eyeRect.height / 2;
 
-				let deltaX = ((mouseX - eyeCenterX) / window.innerWidth) * 200;
-				let deltaY = ((mouseY - eyeCenterY) / window.innerHeight) * 200;
+                let deltaX = ((mouseX - eyeCenterX) / window.innerWidth) * 200;
+                let deltaY = ((mouseY - eyeCenterY) / window.innerHeight) * 200;
 
-				// 检查是否在椭圆形轨迹内
-				if (
-					(deltaX * deltaX) / (maxMovementX * maxMovementX) +
-					(deltaY * deltaY) / (maxMovementY * maxMovementY) >
-					1
-				) {
-					const angle = Math.atan2(deltaY, deltaX);
-					deltaX = maxMovementX * Math.cos(angle);
-					deltaY = maxMovementY * Math.sin(angle);
-				}
+                // 检查是否在椭圆形轨迹内
+                if (
+                    (deltaX * deltaX) / (maxMovementX * maxMovementX) +
+                    (deltaY * deltaY) / (maxMovementY * maxMovementY) >
+                    1
+                ) {
+                    const angle = Math.atan2(deltaY, deltaX);
+                    deltaX = maxMovementX * Math.cos(angle);
+                    deltaY = maxMovementY * Math.sin(angle);
+                }
 
-				eye.style.left = `calc(${initialLeft}% + ${deltaX}%)`;
-				eye.style.top = `calc(${initialTop}% + ${deltaY}%)`;
-			},
-			handleMouseMove(event){
-				requestAnimationFrame(() => {
-					this.moveEye(
-						this.$refs.leftEyeRef,
-						event.clientX,
-						event.clientY,
-						47.8,  // x軸定位點
-						43.2,   // y軸定位點
-						1, // x軸移動範圍
-						1.4 // y軸移動範圍
-					);
-					this.moveEye(
-						this.$refs.rightEyeRef,
-						event.clientX,
-						event.clientY,
-						51.75,  // x軸定位點
-						42.35,   // y軸定位點
-						0.8, // x軸移動範圍
-						1.28 // y軸移動範圍
-					);
-				});
-			}
+                eye.style.left = `calc(${initialLeft}% + ${deltaX}%)`;
+                eye.style.top = `calc(${initialTop}% + ${deltaY}%)`;
+            },
+            handleMouseMove(event) {
+                requestAnimationFrame(() => {
+                    this.moveEye(
+                        this.$refs.leftEyeRef,
+                        event.clientX,
+                        event.clientY,
+                        47.8,  // x軸定位點
+                        43.2,   // y軸定位點
+                        1, // x軸移動範圍
+                        1.4 // y軸移動範圍
+                    );
+                    this.moveEye(
+                        this.$refs.rightEyeRef,
+                        event.clientX,
+                        event.clientY,
+                        51.75,  // x軸定位點
+                        42.35,   // y軸定位點
+                        0.8, // x軸移動範圍
+                        1.28 // y軸移動範圍
+                    );
+                });
+            }
         },
     };
 </script>
