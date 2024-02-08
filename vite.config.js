1f530b6fd564ef2fdde343042449faae31600675
morxdd <pomin1995qoot@gmail.com>
2024-02-08 22:23:06 +0800

    白波浪修正中 首頁banner字體用sass

--- vite.config.js
+++ vite.config.js
@@ -20,5 +20,6 @@ export default defineConfig({
     // host: '192.168.5.136'
     // host: '10.1.19.8', //劉書院的TibaMe_Guest
     // host: '192.168.0.204' //劉書院家的IP
+    // host: '172.20.10.8' //劉書手機熱點的IP
   }
 });
