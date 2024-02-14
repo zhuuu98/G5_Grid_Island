3ab7204416c7270dbfb69a46f2ddfff2be9f68e9
morxdd <pomin1995qoot@gmail.com>
2024-02-14 15:00:48 +0800

    註解ip

--- vite.config.js
+++ vite.config.js
@@ -23,7 +23,7 @@ export default defineConfig({
     // host: '10.1.19.8', //劉書院的TibaMe_Guest
     // host: '192.168.0.204' //劉書院MAC的IP
     // host: '192.168.0.204', //劉書院家的IP
-    host: '172.20.10.8', //劉書手機熱點的IP
+    // host: '172.20.10.8', //劉書手機熱點的IP
     // host: '192.168.1.116', //劉書院的貓的IP
     proxy: {
       // 带选项写法：http://localhost:5173/api/bar.php -> https://tibamef2e.com/cgd103/g1/api/bar.php
